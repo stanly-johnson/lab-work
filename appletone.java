217a47c8a08664683bba0d5a96da1601d5811427
Stanly Johnson <stanly-johnson@users.noreply.github.com>
2016-10-14 11:42:06 +0530

    Add files via upload

---
+++ appletone.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.io.*;
+import java.applet.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+/*<applet code="Applet2a.java" width=900 height=900 name="first"> 
+</applet>*/
+/*<applet code="Applet2b.java" width=900 height=900 name="second"> 
+</applet>*/
+
+public class appletone extends JApplet implements ActionListener
+{
+	JTextField txt1;
+	JButton bt;
+	JTextArea ta1;
+	public void init()
+	{	
+		Container c=getContentPane();
+		c.setLayout(new FlowLayout());
+		txt1= new JTextField(10);
+		c.add(txt1);
+		bt=new JButton("Send");
+		c.add(bt);
+		bt.addActionListener(this);
+		ta1=new JTextArea(20,10);
+		c.add(ta1);
+		
+	}
+	public void actionPerformed(ActionEvent ae)
+	{
+	 	JApplet second1=(JApplet).getAppletContext().getApplet("second");
+		((applettwo)second1).ta2.append("first>"+txt1.getText()+"\n");
+		txt1.append("first>"+txt1.getText()+"\n");
+		txt1.setText("");
+	}
+}	
+
---
+++ applettwo.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.io.*;
+import java.applet.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+
+public class applettwo extends JApplet implements ActionListener
+{
+	JTextField txt2;
+	JButton bt;
+	JTextArea ta2;
+	public void init()
+	{	
+		Container c=getContentPane();
+		c.setLayout(new FlowLayout());
+		txt2= new JTextField(10);
+		c.add(txt2);
+		bt=new JButton(Send);
+		c.add(bt);
+		bt.addActionListener(this);
+		ta1=new JTextArea(20,10);
+		c.add(ta2);
+		
+	}
+	public void actionPerformed(ActionEvent ae)
+	{
+		
+	 	JApplet first1=(JApplet).getAppletContext().getApplet("first");
+		((appletone)first1).ta1.append("second>"+txt2.getText()+"\n");
+		txt1.append("second>"+txt2.getText()+"\n");
+		txt1.setText("");
+	
+	}
+}	
